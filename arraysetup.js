d7db322d2296ea28de8c0c2d3a41e25137408339
Reece Smith <reecesmith4@hotmail.com>
2023-12-30 13:15:20 +1100

    some array outlining

---
+++ arraysetup.js
@@ -0,0 +1,22 @@
+let docArray = [{
+    name: "README",
+    icon: "./images+icons/txt.png",
+    appHash: 1,
+    docContent: "The readme text of this project."
+}];
+
+let picArray = [{
+    name: "Beach",
+    icon: "./images+icons/pic.png",
+    appHash: 2,
+    docContent: "./images+icons/backgrounds/beach.png"
+}];
+
+let deskArray = [{
+    name: "Edge",
+    icon: "./images+icons/edge.png",
+    appHash: 5
+}];
+
+let binArray = [{...deletedItem, loc: oldArray}];
+
