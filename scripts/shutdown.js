2dc9cb8e2f99e73c170e5140be640879e83efd35
Ayoralol <121880251+Ayoralol@users.noreply.github.com>
2024-01-02 09:01:14 +1100

    Added shutdown functionality

---
+++ scripts/shutdown.js
@@ -0,0 +1,10 @@
+let powerButton = document.querySelector(".power--shutdown");
+
+powerButton.addEventListener("click", function () {
+  let body = document.getElementsByTagName("body")[0];
+  let shutdown = confirm("Are you sure you want to shutdown?");
+  if (shutdown) {
+    body.style.backgroundColor = "black";
+    body.innerHTML = "";
+  }
+});
